389cc50cf69bbdbf5317830559eaa89a2f93bed2
Ysann3 <Ysadegraaf@hotmail.com>
2021-06-04 10:33:58 +0200

    load houses and batteries

---
+++ main.py
@@ -0,0 +1,6 @@
+import csv 
+
+district_number = "1"
+
+battery_file = (f"data/Huizen&Batterijen/district_{district_number}/district-{district_number}_batteries.csv")
+house_file = (f"data/Huizen&Batterijen/district_{district_number}/district-{district_number}_houses.csv")
