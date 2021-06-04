6eef81bd6d00e32dea00e42568cc2ae6aa14ae6b
Ysann3 <Ysadegraaf@hotmail.com>
2021-06-04 10:26:07 +0200

    load_house and battery

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -9,25 +9,40 @@ class Grid():
         self.houses = self.load_houses(self, house_file)
         self.batteries = self.load_batteries(self, battery_file)
     
-    def load_houses(self, house_file):
+def load_houses(self, house_file):
         """
-        Load all the houses onto the grid.
+        Load the position of the houses and their max output.
         """
-        # nodes = {}
-        # with open(source_file, 'r') as in_file:
-        #     reader = csv.DictReader(in_file)
+        house_coordinatesX = {}
+        house_coordinatesY = {}
+        house_max_output = {}
+        with open(house_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
 
-        #     for row in reader:
-        #         nodes[row['id']] = Node(row['id'], row['id'])
+            for count, row in enumerate(reader):
+                house_coordinatesX[count+1] = row['x']
+                house_coordinatesY[count+1] = row['x']
+                house_max_output[count+1] = row['maxoutput']
 
-        # return nodes
-        pass
+        return house_coordinatesX, house_coordinatesX, house_max_output
 
-    def load_batteries(self, battery_file):
+def load_batteries(self, battery_file):
         """
-        Load all the batteries onto the grid.
+        Load the positions of all the batteries.
         """
-        pass
+        batteriesX = {}
+        batteriesY = {}
+        batteries_capacity = {}
+        with open(battery_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+
+            for count, row in enumerate(reader):
+                coordinates = row['positie'].split(',')
+                batteriesX[count+1] = coordinates[0]
+                batteriesY[count+1] = coordinates[1]
+                batteries_capacity[count+1] = row['capaciteit']
+
+        return batteriesX, batteriesY, batteries_capacity
 
     def add_cable(self, cable):
         """
