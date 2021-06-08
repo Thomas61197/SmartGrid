d6ce9fa6afb0c4ad3cd1e4679d53aee816287920
Ysann3 <Ysadegraaf@hotmail.com>
2021-06-08 17:08:30 +0200

    greedy algorithm

--- code/algorithms/greedy.py
+++ code/algorithms/greedy.py
@@ -9,26 +9,6 @@ class Greedy:
     def __init__(self, grid):
         self.grid = copy.deepcopy(grid)
 
-    def run(self):
-        """
-        Calculates the Manhattan distances from each house to a battery
-        """
-        
-        # Create a dictionary of all the houses with a list of the Manhattan distances to each battery
-        for house in self.grid.houses.values():
-            distances = {}
-
-            for battery in self.grid.batteries.values():
-                distances[battery.id] = abs(house.x - battery.x) + abs(house.y - battery.y)
-            # Sort distance from low to high
-            distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
-            closest_battery = self.grid.batteries[next(iter(distances.keys()))]
-            closest_battery.add_house(house)
-
-
-
-
-
     # alternative
     def get_man_distance(self):
         """
@@ -58,33 +38,65 @@ class Greedy:
             for house in distances:
                 if distances[house][0][0] is battery:
                     battery_distances[battery][house] = distances[house][0][1]
-            battery_distances[battery] = sorted(battery_distances[battery].items(), key=lambda x: x[1])
+            # Ordered is not neccessary anymore
+            #battery_distances[battery] = sorted(battery_distances[battery].items(), key=lambda x: x[1])
             battery.houses = battery_distances[battery]
 
         return battery_distances
 
+    def replace_connections(self): 
+        """
+        Find the houses with the shortest distance to their second closest battery, 
+        connect if capacity of current battery is filled
+        """
+        self.battery_distance_list()
+
+        for battery in self.grid.batteries():
+            while battery.capacity_reached():
+                # Make a dict of distances from houses to their next closest battery
+                second_closest_battery = {}
+                for house in battery.houses:
+                    house_id = house[0]
+                    house_obj = self.grid.houses[house_id]
+                    second_closest = house_obj.battery_distances[(house_obj.rank + 1)]
+                    second_closest_battery[house_id] = second_closest
+                # Sort that dict
+                second_closest_battery = sorted(second_closest_battery.items(), key=lambda x: x[1])
+                # Pick first one as house to ditch
+                replace_house = second_closest_battery[0][0]
+                replace_to_battery = second_closest_battery[0][1]
+
+                # Remove found house from current battery
+                battery.houses.pop(replace_house)
+                # Indicate with rank that we've changed batteries
+                self.grid.houses[replace_house].rank += 1
+                # Add house to new battery
+                self.grid.batteries[replace_to_battery].add_house(replace_house)
+
+                # If summed power is larger than capacity of battery, ditch the house with largest distance:
+                # The ditched house gets connected to the second (or third etc) closest battery
+                # Pop the last item in the list
+                #to_be_popped = battery.houses[-1]
+                #battery.houses.pop()
+                # Connect popped house to second closest battery
+                #house_id = to_be_popped[0]
+                #self.grid.houses.get(house_id)
+                #self.grid.houses[]
+                #house.rank = house.rank + 1
+                #battery_id = house.battery_distances[house.rank][1]
+                # Get battery object somehow
+                #add_to_battery = get.battery[battery_id]
+                #add_to_battery.add_house(house_id)
+
     def run_greedy(self): ## TO BE FIXED
         """
-        Reallocate houses to batteries if max capacity of a battery is reached
+        Find the houses with the shortest distance to their second closest battery, 
+        connect if capacity of current battery is filled
         """
-        # If summed power is larger than capacity of battery, ditch the house with largest distance:
-        # The ditched house gets connected to the second (or third etc) closest battery
-        for battery in self.grid.batteries.values():
-            while battery.capacity_reached():
-                # Pop the last item in the list
-                to_be_popped = battery.houses[-1]
-                battery.houses.pop()
-                # Connect popped house to second closest battery
-                house_id = to_be_popped[0]
-
-                self.grid.houses.get(house_id)
-                self.grid.houses[]
-                house.rank = house.rank + 1
-                battery_id = house.battery_distances[house.rank][1]
-                # Get battery object somehow
-                add_to_battery = get.battery[battery_id]
-                add_to_battery.add_house(house_id)
-
+        self.replace_connections()
+        for house in self.grid.houses():
+            closest_battery_id = house.battery_distances[house.rank][0]
+            closest_battery = self.grid.batteries[closest_battery_id]
 
             # determine cable location
             x = list()
@@ -132,7 +144,7 @@ class Greedy:
                     x.append(cable_head_x)
                     y.append(cable_head_y)
 
-            house.add_cable(cable.Cable(x, y, house, closest_battery, distances[closest_battery.id]))
+            house.add_cable(cable.Cable(x, y, house, closest_battery, house.battery_distances[house.rank][1]))
         
         return self.grid
 
--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from code.classes import grid
-from code.algorithms import random, original_greedy
+from code.algorithms import random, original_greedy, greedy
 import json
 from code.visualisations import visualise_costs
 
 if __name__ == "__main__":
     district_number = "1"
 
-    battery_file = (f"SmartGrid/data/Huizen&Batterijen/district_{district_number}/district-{district_number}_batteries.csv")
+    battery_file = (f"data/Huizen&Batterijen/district_{district_number}/district-{district_number}_batteries.csv")
+
+    #battery_file = (f"SmartGrid/data/Huizen&Batterijen/district_{district_number}/district-{district_number}_batteries.csv")
     # battery_file = "/home/thomas61197/SmartGrid/data/Huizen&Batterijen/district_1/district-1_batteries.csv"
 
-    house_file = (f"SmartGrid/data/Huizen&Batterijen/district_{district_number}/district-{district_number}_houses.csv")
+    house_file = (f"data/Huizen&Batterijen/district_{district_number}/district-{district_number}_houses.csv")
+
+    #house_file = (f"SmartGrid/data/Huizen&Batterijen/district_{district_number}/district-{district_number}_houses.csv")
     # house_file = "/home/thomas61197/SmartGrid/data/Huizen&Batterijen/district_1/district-1_houses.csv"
 
     grid1 = grid.Grid(house_file, battery_file)
 
     # --------------------------- random --------------------------
-    random_costs = list()
+    # random_costs = list()
 
-    for i in range(1000):
-        random1 = random.Random(grid1)
-        grid2 = random1.run()
-        random_costs.append(grid2.calc_cost())
+    # for i in range(1000):
+    #     random1 = random.Random(grid1)
+    #     grid2 = random1.run()
+    #     random_costs.append(grid2.calc_cost())
     
-    visualise_costs.visualise_costs(random_costs, "random")
+    # visualise_costs.visualise_costs(random_costs, "random")
     
     # --------------------------- greedy --------------------------
     greedy_costs = list()
@@ -34,6 +38,15 @@ if __name__ == "__main__":
 
     visualise_costs.visualise_costs(greedy_costs, "greedy")
 
+ # --------------------------- greedy 2 --------------------------
+    greedy2_costs = list()
+    
+    for i in range(1000):
+        greedy2 = greedy.Greedy(grid1)
+        grid4 = greedy2.run()
+        greedy2_costs.append(grid4.calc_cost())
+
+    visualise_costs.visualise_costs(greedy2_costs, "greedy2")
     # --------------------------- compare --------------------------
 
     visualise_costs.compare_costs(random_costs, "random", greedy_costs, "greedy")
